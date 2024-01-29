29cd541a61b4e8429809753a668768349cfc3442
Xuan <daoxuandung010302@gmail.com>
2024-01-29 13:18:38 +0700

    Shopee

---
+++ src/msw/auth.msw.ts
@@ -0,0 +1,51 @@
+import { rest } from 'msw'
+import config from 'src/constants/config'
+import HttpStatusCode from 'src/constants/httpStatusCode.enum'
+
+export const access_token_1s =
+  'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNmY5MzVlNWZkYzVmMDM3ZTZmNjhkMyIsImVtYWlsIjoiZDNAZ21haWwuY29tIiwicm9sZXMiOlsiVXNlciJdLCJjcmVhdGVkX2F0IjoiMjAyMi0xMi0xNVQxNDowMzoyMy41NzdaIiwiaWF0IjoxNjcxMTEzMDAzLCJleHAiOjE2NzExMTMwMDR9.-gQIpbbKFlRqBlpiiAOBD4puP8jcMtZ2lobXPcy1zmU'
+export const refresh_token_1000days =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNmY5MzVlNWZkYzVmMDM3ZTZmNjhkMyIsImVtYWlsIjoiZDNAZ21haWwuY29tIiwicm9sZXMiOlsiVXNlciJdLCJjcmVhdGVkX2F0IjoiMjAyMi0xMi0xNVQxNDowNTozNS41MTVaIiwiaWF0IjoxNjcxMTEzMTM1LCJleHAiOjE3NTc1MTMxMzV9.OHDBqBjhih1fgNe6-mWo0PQ-IcukNz4ljlXUCxM-8V8'
+export const access_token =
+  'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNmY5MzVlNWZkYzVmMDM3ZTZmNjhkMyIsImVtYWlsIjoiZDNAZ21haWwuY29tIiwicm9sZXMiOlsiVXNlciJdLCJjcmVhdGVkX2F0IjoiMjAyMi0xMi0xOVQxMzo1ODo0OC40ODlaIiwiaWF0IjoxNjcxNDU4MzI4LCJleHAiOjE2ODE0NTgzMjd9.00oi-93dF4Wz2Ngb6_G2dXO4VQXf2cRCft3W8DKgPdA'
+const loginRes = {
+  message: 'Đăng nhập thành công',
+  data: {
+    access_token:
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNmY5MzVlNWZkYzVmMDM3ZTZmNjhkMyIsImVtYWlsIjoiZDNAZ21haWwuY29tIiwicm9sZXMiOlsiVXNlciJdLCJjcmVhdGVkX2F0IjoiMjAyMi0xMi0xOVQwNDoxODowMC4wNjRaIiwiaWF0IjoxNjcxNDIzNDgwLCJleHAiOjE2NzI0MjM0Nzl9.AxOvjaTErYwvOSdMWtZgefX8JJ3KaMCZWNCj72uqzYY',
+    expires: 999999,
+    refresh_token:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNmY5MzVlNWZkYzVmMDM3ZTZmNjhkMyIsImVtYWlsIjoiZDNAZ21haWwuY29tIiwicm9sZXMiOlsiVXNlciJdLCJjcmVhdGVkX2F0IjoiMjAyMi0xMi0xOVQwNDoxODowMC4wNjRaIiwiaWF0IjoxNjcxNDIzNDgwLCJleHAiOjE3NTc4MjM0ODB9.AvavrdIeU1xm2KrFeEKSiDJs260YU1uWxRzVw30MgoU',
+    expires_refresh_token: 86400000,
+    user: {
+      _id: '636f935e5fdc5f037e6f68d3',
+      roles: ['User'],
+      email: 'daoxuandung010302@gmail.com',
+      createdAt: '2024-01-01T12:36:46.282Z',
+      updatedAt: '2024-01-10T07:57:45.069Z',
+      __v: 0,
+      avatar: '',
+      name: 'Dao Xuan Dung'
+    }
+  }
+}
+
+const refreshTokenRes = {
+  message: 'Refresh Token thành công',
+  data: {
+    access_token:
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNmY5MzVlNWZkYzVmMDM3ZTZmNjhkMyIsImVtYWlsIjoiZDNAZ21haWwuY29tIiwicm9sZXMiOlsiVXNlciJdLCJjcmVhdGVkX2F0IjoiMjAyMi0xMi0xOVQwNzozMTowMC4yNTJaIiwiaWF0IjoxNjcxNDM1MDYwLCJleHAiOjE2NzIwMzk4NjB9.vTHglpuxad5h_CPpIaDCUpW0xJPYarJzLFeeul0W61E'
+  }
+}
+
+const loginRequest = rest.post(`${config.baseUrl}login`, (req, res, ctx) => {
+  return res(ctx.status(HttpStatusCode.Ok), ctx.json(loginRes))
+})
+
+const refreshToken = rest.post(`${config.baseUrl}refresh-access-token`, (req, res, ctx) => {
+  return res(ctx.status(HttpStatusCode.Ok), ctx.json(refreshTokenRes))
+})
+
+const authRequests = [loginRequest, refreshToken]
+
+export default authRequests
---
+++ src/msw/user.msw.ts
@@ -0,0 +1,39 @@
+import { rest } from 'msw'
+import config from 'src/constants/config'
+import HttpStatusCode from 'src/constants/httpStatusCode.enum'
+import { access_token_1s } from './auth.msw'
+
+const meRes = {
+  message: 'Lấy người dùng thành công',
+  data: {
+    _id: '636f935e5fdc5f037e6f68d3',
+    roles: ['User'],
+    email: 'daoxuandung010302@gmail.com',
+    createdAt: '2024-01-01T12:36:46.282Z',
+    updatedAt: '2024-01-10T07:57:45.069Z',
+    __v: 0,
+    avatar: '',
+    name: 'Dao Xuan Dung'
+  }
+}
+
+const meRequest = rest.get(`${config.baseUrl}me`, (req, res, ctx) => {
+  const access_token = req.headers.get('authorization')
+  if (access_token === access_token_1s) {
+    return res(
+      ctx.status(HttpStatusCode.Unauthorized),
+      ctx.json({
+        message: 'Lỗi',
+        data: {
+          message: 'Token hết hạn',
+          name: 'EXPIRED_TOKEN'
+        }
+      })
+    )
+  }
+  return res(ctx.status(HttpStatusCode.Ok), ctx.json(meRes))
+})
+
+const userRequests = [meRequest]
+
+export default userRequests
